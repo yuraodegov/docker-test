5f3a63e495a724e6fd3cda64b8f5ecf2cb000d52
yuraodegov <yuraodegov@gmail.com>
2026-04-14 07:09:58 +0000

    fix: remove build folder from git

--- my_project/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/docker-test/my_project
-# Build directory: /workspaces/docker-test/my_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ButtonTests "/workspaces/docker-test/my_project/build/test_button")
-set_tests_properties(ButtonTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/docker-test/my_project/CMakeLists.txt;7;add_test;/workspaces/docker-test/my_project/CMakeLists.txt;0;")
